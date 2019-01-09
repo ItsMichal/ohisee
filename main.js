4ecad15bce3d334e8a08386e962ffd998e8a7d99
itsMichal <michaljbodzianowski@gmail.com>
2019-01-08 22:11:32 -0700

    woops

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ filter.removeWords("shit", "hell", "heck", "damn");
 var twitch = new TwitchBot({
   username: 'ohiseebot',
   oauth: '***REMOVED***',
-  channels: ['supersaiyanlib']
+  channels: ['supersaiyanlilb']
 });
 
 // try{
